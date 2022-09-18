6e297e2d81fb9496da67ee142956c569f99ec6dd
Julien C <hoopsinet@hotmail.fr>
2022-09-18 20:57:34 +0200

    modification dans  lapps > access control et helmet

--- backend/app.js
+++ backend/app.js
@@ -20,14 +20,15 @@ const app = express();
 
 //header settings
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Cross-Origin-Opener-Policy', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
 app.use(express.json());
-app.use(helmet());
+app.use(helmet({crossOriginResourcePolicy: false,}));
 
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', saucesRoutes);
